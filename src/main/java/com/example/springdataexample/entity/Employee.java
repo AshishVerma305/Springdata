b22010d786e4c70d9d735f014abda6866441454b
ashishverma <ashishvermahell4@gmail.com>
2021-02-23 18:17:23 +0530

    redis

--- src/main/java/com/example/springdataexample/entity/Employee.java
+++ src/main/java/com/example/springdataexample/entity/Employee.java
@@ -2,22 +2,14 @@ package com.example.springdataexample.entity;
 
 import lombok.Getter;
 import lombok.Setter;
-import org.hibernate.annotations.GenericGenerator;
-
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-
-@Entity
+import org.springframework.data.annotation.Id;
+import org.springframework.data.redis.core.RedisHash;
+@RedisHash
 @Getter
 @Setter
 public class Employee {
     @Id
-    @GenericGenerator(name = "employee_id_seq", strategy = "increment")
-    @GeneratedValue(generator = "employee_id_seq", strategy = GenerationType.AUTO)
     private long id;
     private String name;
     private String departmentName;
-
 }
